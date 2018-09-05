285ee4ab0307dd09e73d23b3036c845c427653e2
labo <labo@ort.edu.uy>
2018-09-05 20:39:46 -0300

    FileReader TDD example - Final

---
+++ FileReader.FileAccess/FileOpener.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReader.FileAccess
+{
+    public class FileOpener
+    {
+        public void OpenFile(string path)
+        {
+            try
+            {
+                TryOpenFile(path);
+            } catch (FileNotFoundException ex)
+            {
+                throw new FileOpenerFailedException(ex);
+            }
+        }
+
+        private void TryOpenFile(string path)
+        {
+            StreamReader reader = new StreamReader(path);
+        }
+    }
+}
---
+++ FileReader.FileAccess/FileOpenerFailedException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace FileReader.FileAccess
+{
+    public class FileOpenerFailedException : Exception
+    {
+        private const string MESSAGE = "ERROR AL ABRIR EL ARCHIVO";
+        public FileOpenerFailedException() : base(MESSAGE)
+        {
+        }
+        
+        public FileOpenerFailedException(Exception innerException) : base(MESSAGE, innerException)
+        {
+        }
+
+    }
+}
